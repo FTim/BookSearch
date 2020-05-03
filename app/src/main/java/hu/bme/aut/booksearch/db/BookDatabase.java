cfd98ce5bf0d6089f651653d47d0f28482c41ffe
FTim <32366965+FTim@users.noreply.github.com>
2020-05-03 21:54:00 +0200

    Added model annotations for DB

--- app/src/main/java/hu/bme/aut/booksearch/db/BookDatabase.java
+++ app/src/main/java/hu/bme/aut/booksearch/db/BookDatabase.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.RoomDatabase;
 
 import hu.bme.aut.booksearch.model.Book;
 
-@Database(entities = {Book.class}, version = 1)
+@Database(entities = {Book.class}, version = 1, exportSchema = false)
 public abstract class BookDatabase extends RoomDatabase {
     public abstract BookDao bookDao();
 }
